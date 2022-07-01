573efd55272c7f2d04e556d660faffe2356e509a
Joseph Macharia <37827513+webmaster254@users.noreply.github.com>
2022-07-01 13:34:45 +0300

    3 alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,9 +10,9 @@ int main(void)
 	char a;
 
 	for (a = 'a'; a <= 'z' ; a++)
-	putchar(a);
+		putchar(a);
 	for (a = 'A'; a <= 'Z'; a++)
-	putchar(a);
+		putchar(a);
 	putchar('\n');
 	return (0);
 }
