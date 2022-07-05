9496f3bd2af2cedb17661e2adc5572fe13709001
Joseph <josephhackeer254@gmail.com>
2022-07-05 03:12:12 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,9 @@
-#include <unistd.h>
-
+#ifndef _MAIN_H
+#define _MAIN_H_
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
+#endif
