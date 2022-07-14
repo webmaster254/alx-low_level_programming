dc7cdb4b8fca7ae9253cea7a4fdf0b150cd76117
Joseph Macharia <37827513+webmaster254@users.noreply.github.com>
2022-07-15 00:29:36 +0300

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -280,4 +280,6 @@ void swap_int(int *a, int *b);
  * Return: returns length
  */
 int _strlen(char *s);
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 #endif
