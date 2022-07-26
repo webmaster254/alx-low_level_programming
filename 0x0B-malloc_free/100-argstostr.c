7399b777c920e29c34e5e026a84e6c56ef9ab97e
Joseph Macharia <37827513+webmaster254@users.noreply.github.com>
2022-07-26 12:58:57 -0700

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,45 +1,37 @@
 #include "main.h"
-#include <stdlib.h>
-
 /**
- * argstostr - Concatenates all arguments of the program into a string;
- *             arguments are separated by a new line in the string.
- * @ac: The number of arguments passed to the program.
- * @av: An array of pointers to the arguments.
- *
- * Return: If ac == 0, av == NULL, or the function fails - NULL.
- *         Otherwise - a pointer to the new string.
- */
+ * argstostr - Concatenate all arguements
+ * @ac: Number of arguements
+ * @av: Pointer to string arguements
+ * Return: Pointer to new string
+ **/
 char *argstostr(int ac, char **av)
 {
-  char *str;
-  int arg, byte, index, size = ac;
+	char *cont;
+	int i;
+	int j;
+	int len = 0;
 
-  if (ac == 0 || av == NULL)
-    return (NULL);
-
-  for (arg = 0; arg < ac; arg++)
-    {
-      for (byte = 0; av[arg][byte]; byte++)
-	size++;
-    }
-
-  str = malloc(sizeof(char) * size + 1);
-
-  if (str == NULL)
-    return (NULL);
-
-  index = 0;
-
-  for (arg = 0; arg < ac; arg++)
-    {
-      for (byte = 0; av[arg][byte]; byte++)
-	str[index++] = av[arg][byte];
-
-      str[index++] = '\n';
-    }
-
-  str[size] = '\0';
-
-  return (str);
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j++])
+			len++;
+	}
+	len++;
+	cont = malloc(sizeof(**av) * (len + ac));
+	if (cont == NULL)
+		return (NULL);
+	len = 0;
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j])
+			cont[len++] = av[i][j++];
+		cont[len++] = '\n';
+	}
+	cont[len] = '\0';
+	return (cont);
 }
