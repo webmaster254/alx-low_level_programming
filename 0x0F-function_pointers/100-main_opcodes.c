9ad366a23dce2bef10b08c570180b0169272a016
Joseph Macharia <37827513+webmaster254@users.noreply.github.com>
2022-08-03 16:25:13 -0700

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,44 +2,38 @@
 #include <stdlib.h>
 
 /**
- * main - prints the opcodes of itself
- * @argc: the number of args
- * @argv: arg vector
- * Return: Always 0
+ * main - check the code for Alx School Student
+ * * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-  int bytes, index;
-  int (*address)(int, char **) = main;
-  unsigned char opcode;
+	char *opc = (char *) main;
+	int i, nbytes;
 
-  if (argc != 2)
-    {
-      printf("Error\n");
-      exit(1);
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-  bytes = atoi(argv[1]);
+	nbytes = atoi(argv[1]);
 
-  if (bytes < 0)
-    {
-      printf("Error\n");
-      exit(2);
-    }
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
-  for (index = 0; index < bytes; index++)
-    {
-      opcode = *(unsigned char *)address;
-      printf("%.2x", opcode);
+	for (i = 0; i < nbytes; i++)
+	{
+		printf("%02x", opc[i] & 0xFF);
+		if (i != nbytes - 1)
+			printf(" ");
+	}
 
-      if (index == bytes - 1)
-	continue;
-      printf(" ");
-
-      address++;
-    }
-
-  printf("\n");
-
-  return (0);
+	printf("\n");
+	return (0);
 }
